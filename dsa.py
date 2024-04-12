6c7a7e7c23dd38e74388121d246331e8de44d03d
Yawksh <152399244+Yawksh@users.noreply.github.com>
2024-04-12 23:37:03 +0300

    Update dsa.py

--- dsa.py
+++ dsa.py
@@ -6,8 +6,6 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-        
-
     def print(self):
         if self.head is None:
             print("Linked list is empty")
