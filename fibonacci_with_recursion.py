5072cf0f113acd1f109aa704e74045bcabe2b24e
Yawksh <152399244+Yawksh@users.noreply.github.com>
2024-04-25 05:19:38 +0300

    Create fibonacci_with_recursion.py

---
+++ fibonacci_with_recursion.py
@@ -0,0 +1,10 @@
+'''Implementation Using Recursion
+Recursion is when a function calls itself.
+
+To implement the Fibonacci algorithm we need most of the same things as in the code by using looping, 
+but we need to replace the for loop with recursion.
+
+To replace the for loop with recursion, we need to encapsulate much of the code in a function, 
+and we need the function to call itself to create a new Fibonacci number as long as the produced number of Fibonacci numbers is below, 
+or equal to, n-2 n is the number of fibonacci numbers
+for this eaxple we will generate 20 numbers'''
