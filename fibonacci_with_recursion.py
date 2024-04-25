b401a5caf85b2e3af1e209b7e9682b84142a5388
Yawksh <152399244+Yawksh@users.noreply.github.com>
2024-04-25 05:45:24 +0300

    Update fibonacci_with_recursion.py

--- fibonacci_with_recursion.py
+++ fibonacci_with_recursion.py
@@ -8,3 +8,23 @@ To replace the for loop with recursion, we need to encapsulate much of the code 
 and we need the function to call itself to create a new Fibonacci number as long as the produced number of Fibonacci numbers is below, 
 or equal to, n-2 n is the number of fibonacci numbers
 for this eaxple we will generate 20 numbers'''
+print(0)
+print(1)
+count = 2
+
+def fibonacci(prev1, prev2):
+    global count
+    if count <= 19:
+        newFibo = prev1 + prev2
+        print(newFibo)
+        prev2 = prev1
+        prev1 = newFibo
+        count += 1
+        fibonacci(prev1, prev2)
+    else:
+        return
+
+fibonacci(1,0)
+
+''' the output will be 
+0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 '''
