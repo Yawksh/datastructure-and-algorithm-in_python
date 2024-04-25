f1c7bc1ebb72bba1ad6bf272b252651c4b60a223
Yawksh <152399244+Yawksh@users.noreply.github.com>
2024-04-25 05:11:18 +0300

    Update fibonacci_with_loop.py

--- fibonacci_with_loop.py
+++ fibonacci_with_loop.py
@@ -1,3 +1,13 @@
+'''Implementation Using a For Loop
+It can be a good idea to list what the code must contain or do before programming it:
+
+Two variables to hold the previous two Fibonacci numbers
+A for loop that runs 18 times
+Create new Fibonacci numbers by adding the two previous ones
+Print the new Fibonacci number
+Update the variables that hold the previous two fibonacci numbers
+Using the list above, it is easier to write the program:'''
+
 prev2 = 0
 prev1 = 1
 
